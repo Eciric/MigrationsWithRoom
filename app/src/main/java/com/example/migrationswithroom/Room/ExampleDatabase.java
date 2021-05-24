2842656541da77b2d599b2fcc36b5c8a57d35ce2
Krystian Kopeć <73670385+Cannibal90@users.noreply.github.com>
2021-05-25 01:38:04 +0200

    add ExampleDb Impl

---
+++ app/src/main/java/com/example/migrationswithroom/Room/ExampleDatabase.java
@@ -0,0 +1,40 @@
+package com.example.migrationswithroom.Room;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.migrationswithroom.Room.dao.CategoryDao;
+import com.example.migrationswithroom.Room.dao.ItemDao;
+import com.example.migrationswithroom.Room.dao.OwnerDao;
+import com.example.migrationswithroom.Room.model.Category;
+import com.example.migrationswithroom.Room.model.Item;
+import com.example.migrationswithroom.Room.model.Owner;
+
+@Database(entities = {Category.class, Item.class, Owner.class}, version = 1, exportSchema = false)
+public abstract class ExampleDatabase extends RoomDatabase {
+
+    public abstract CategoryDao categoryDao();
+
+    public abstract ItemDao itemDao();
+
+    public abstract OwnerDao ownerDao();
+
+    private static  ExampleDatabase INSTANCE;
+
+    static ExampleDatabase getDatabase(final Context context){
+        if(INSTANCE == null){
+            synchronized (ExampleDatabase.class){
+                if(INSTANCE == null){
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), ExampleDatabase.class, "example_db")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+}
